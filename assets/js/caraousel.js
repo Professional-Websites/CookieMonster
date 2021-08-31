b25fb08e33c0f744ddd4565e50dcaca6ac38d445
Professional-Websites <89712320+Professional-Websites@users.noreply.github.com>
2021-08-31 21:22:17 +0530

    Add files via upload

---
+++ assets/js/caraousel.js
@@ -0,0 +1,24 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    autoplay: false,
+    autoplayHoverPause: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      800: {
+        items: 3
+      },
+      1000: {
+        items: 4
+      }
+    }
+  });
+  
+
+  
